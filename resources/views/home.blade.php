8678381094e1a23941c8480c828e98b853b63c33
maulanafanny <maulanafanny38@gmail.com>
2022-07-12 15:06:13 +0700

    added catalog cards to homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -19,5 +19,51 @@
             </div>
         </div>
     </div>
+</div> --}}
+<div class="container-fluid">
+    <h2 class="text-center fw-bold">Katalog Kami</h2>
+    <section class="catalog-card-list">
+        <a href="#" class="catalog-card shadow text-decoration-none">
+            <header class="catalog-card-header">
+                <p>14 Juni 2022</p>
+                <h2>Card Tricks, Fanny Maulana Rizky</h2>
+            </header>
+        </a>
+        
+        <a href="#" class="catalog-card shadow text-decoration-none">
+            <header class="catalog-card-header">
+                <p>14 Juni 2022</p>
+                <h2>Card Tricks, Fanny Maulana Rizky</h2>
+            </header>
+        </a>
+        
+        <a href="#" class="catalog-card shadow text-decoration-none">
+            <header class="catalog-card-header">
+                <p>14 Juni 2022</p>
+                <h2>Card Tricks, Fanny Maulana Rizky</h2>
+            </header>
+        </a>
+
+        <a href="#" class="catalog-card shadow text-decoration-none">
+            <header class="catalog-card-header">
+                <p>14 Juni 2022</p>
+                <h2>Card Tricks, Fanny Maulana Rizky</h2>
+            </header>
+        </a>
+        
+        <a href="#" class="catalog-card shadow text-decoration-none">
+            <header class="catalog-card-header">
+                <p>14 Juni 2022</p>
+                <h2>Card Tricks, Fanny Maulana Rizky</h2>
+            </header>
+        </a>
+        
+        <a href="#" class="catalog-card shadow text-decoration-none">
+            <header class="catalog-card-header">
+                <p>14 Juni 2022</p>
+                <h2>Card Tricks, Fanny Maulana Rizky</h2>
+            </header>
+        </a>
+    </section>
 </div>
 @endsection
