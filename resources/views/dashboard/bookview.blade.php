0747da0a6fdd3771c8bf0cb60c60f982efbe8ac9
maulanafanny <maulanafanny38@gmail.com>
2022-07-15 19:34:24 +0700

    improved responsiveness to smaller screens

--- resources/views/dashboard/bookview.blade.php
+++ resources/views/dashboard/bookview.blade.php
@@ -1,23 +1,22 @@
 @extends('dashboard.dashboard')
 
 @section('content')
-    <div style="display:flex; width: 70%; margin: auto; margin-top: 100px; margin-bottom: 100px;">
-        <div style="display: flex; width:250px; height: 370px; margin-top: auto; margin-bottom: auto; margin-right: 20px;">
-            <img class="imgcol shadow-lg" src="{{ $book->cover }}" style="display: flex; width:250px; height: 370px;">
-        </div>
-        <div style="padding-left: 40px">
-            <p class="text" style="color: #212529; padding-top: 8px;">{{ $book->pengarang }}</p>
-            <h1 style="font-weight: bold; font-size: 38px;">{{ $book->judul }}</h1>
-            <hr
-                style="background-color: crimson; width: 25%;  border: none; height: 3px; position: relative; display: inline-block;">
-            <p class="text" style="color: #93a6ab; font-size: 14px; margin-bottom: 20px;">
-                {{ $book->deskripsi }}
-            </p>
-            <div>
-                <h6 class="fs-5 pe-5 fw-bold" style="display: inline-block;">
-                    Rp {{ number_format($book->harga,2,",",".") }}
-                </h6>
-            </div>
+<div class="row justify-content-center container align-items-center" style="margin: 100px auto;">
+    <div class="col-lg-3 col-sm-12 col-md-5 text-center product-cover">
+        <img class="shadow img-fluid" src="{{ $book->cover }}">
+    </div>
+    <div class="col-lg-8 col-sm-12 col-md-7">
+        <p class="" style="color: #212529;">{{ $book->pengarang }}</p>
+        <h1 class="fw-bold" style="font-size: 38px;">{{ $book->judul }}</h1>
+        <hr style="background-color: crimson; width: 25%;  border: none; height: 3px; position: relative; display: inline-block;">
+        <p style="color: #93a6ab; font-size: 14px; margin-bottom: 20px;">
+            {{ $book->deskripsi }}
+        </p>
+        <div>
+            <h6 class="fs-5 pe-5 fw-bold" style="display: inline-block;">
+                Rp {{ number_format($book->harga,2,",",".") }}
+            </h6>
         </div>
     </div>
+</div>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
 <div class="hero">
     <div class="container hero-text">
         <div class="row">
-            <div class="col-5">
+            <div class="col-lg-5 col-sm-12">
                 <h1 style="line-height: 50px">
                     Selamat Datang di Bookslide <br>
                     Brand New & Used
@@ -19,7 +19,7 @@
                     Katalog Kami
                 </a>
             </div>
-            <div class="col-7">
+            <div class="col-7 d-none d-lg-block">
                 <img src="{{ asset('img/book-hero.png') }}" style="max-width: 800px">
             </div>
         </div>
@@ -34,7 +34,7 @@
     <h1 class="text-center fw-bold">Mengapa Memilih Bookslide?</h1>
     <br><br>
     <div class="row justify-content-center">
-        <div class="col-3 me-5">
+        <div class="col-lg-3 col-sm-12 col-md-12 mb-3 me-5">
             <h3 class="fw-semibold mb-4"><i class="bi bi-truck me-3 fs-3"></i>Pengiriman Gratis</h3>
             <p class="hero-text-small" style="color: black">
                 Bookslide menawarkan pengiriman gratis ke seluruh wilayah
@@ -43,7 +43,7 @@
                 Proses pengiriman cepat dan aman.
             </p>
         </div>
-        <div class="col-3 me-5">
+        <div class="col-lg-3 col-sm-12 col-md-12 mb-3 me-5">
             <h3 class="fw-semibold mb-4"><i class="bi bi-tags me-3 fs-3"></i>Diskon & Promo</h3>
             <p class="hero-text-small" style="color: black">
                 Dapatkan penawaran menarik dan promo terbatas dari kami.
@@ -52,7 +52,7 @@
                 yang diperbarui setiap hari.
             </p>
         </div>
-        <div class="col-3 me-5">
+        <div class="col-lg-3 col-sm-12 col-md-12 mb-3 me-5">
             <h3 class="fw-semibold mb-4"><i class="bi bi-patch-check me-3 fs-3"></i></i>Kepuasan Terjamin</h3>
             <p class="hero-text-small" style="color: black">
                 Kami berharap Anda menyukai layanan pembelian buku kami. Tim 
@@ -64,7 +64,6 @@
     </div>
 </div>
 
-<br>
 <br>
 <br>
 <br>
